297a9c85b64ff2ead3fa338baf25a3ca85cd6cca
ish <93941745+ishies@users.noreply.github.com>
2025-04-12 11:34:10 -0500

    Update script.js

---
+++ script.js
@@ -0,0 +1,8 @@
+const questions = {
+    "question1": {
+        question: "What is the capital of France?",
+        options: ["Berlin", "Madrid", "Paris", "Rome"],
+    }
+}
+
+const demoAnswers = [0, 3, 1, 2, 3, 2 , 2, 3, 3, 2, 1, 0, 3, 2, 1, 0, 3, 2, 1, 0];
